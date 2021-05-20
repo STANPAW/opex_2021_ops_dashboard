82124eaa4cb5ce19214e76234585b9f242eea555
STANPAW <64492978+STANPAW@users.noreply.github.com>
2021-05-20 13:18:06 -0500

    Revised Pronum Query to run faster

--- pronumber_Employee_check_Orderauditlog.sql
+++ pronumber_Employee_check_Orderauditlog.sql
@@ -1,17 +1,38 @@
-with New_Pronum_logic -- Temp for testing
-             (Loadnum, Activity, ActivityDateTime, ActivityCount,Employee,ReasonCode,IsAutomated,LoadBasedActivity,SourceAsOfTS) as 
 
-(SELECT DISTINCT 
+ With  ORDERAUDIT_PRONUM As
+            (
+            SELECT
+             OA.entityID  LOADNUM --Loadnum
+             ,Upper(Trim(OA.ENTEREDBYPARTYCODE)) ENTEREDBY
+             ,OA.NewValue  PRONUM     
+            
+            FROM
+            OPERATIONS_DOMAIN.DBO.STG_SHIPMENT_ACTIVITY_LOADLIST LOAD_CTL 
+            INNER JOIN  ORION_RAP.OA.ORDERAUDIT OA on OA.entityID = LOAD_CTL.LOADNUM                                -- matching load num
+
+            WHERE 
+                   OA.EntityTypeID = 2                                               -- 2 = loadnum 1 = ordernum
+               AND OA.ACTIONITEMTYPEID  in (5,31)                                    --- 5 = addrefnum -- 31 Update refnum
+               AND OA.FIELDNAME = 'Value'
+               AND OA.SOURCESYSTEMID in (479,3956)                                    -- 479 =manual entry and 3956 = execution(done in load)
+               AND OA.FIELDPARENT like ('T%')
+               AND OA.EnteredDate >='2021-01-01' and OA.EnteredDate < '2021-04-01' ---- For testing
+            )
+
+,New_Pronum_logic -- Temp for testing
+             (Loadnum, Activity, ActivityDateTime, ActivityCount,Employee,ReasonCode,IsAutomated,LoadBasedActivity,SourceAsOfTS) as 
+(
+SELECT DISTINCT 
             TCA.KeyColumnValue LoadNum
             ,'PRONUM ENTRIES' Activity
             ,TCA.AuditDatetime ActivityDateTime
             ,1 ActivityCount
             ,Upper(Trim(TCA.AuditUserId)) Employee
             ,Upper(Trim(TCA.AuditType)) ReasonCode
-            ,(case  when TCA.AUDITCOLUMNNEWVALUE is null then 0 -- at times users remove the order number leaving the field blank so if new value is null its manual activity. 
-                    when Upper(Trim(OA.ENTEREDBYPARTYCODE)) is null then 1 else 0 end) as IsAutomated -- when order audit as a manual entry is not found then automate
+            ,(case  when TCA.AUDITCOLUMNNEWVALUE is null then Null                                          -- at times users remove the order number leaving the field blank so if new value is null its manual activity. 
+                    when Upper(Trim(OA.ENTEREDBY)) is null then TRUE else NULL end) as IsAutomated    -- when order audit as a manual entry is not found then automate
            
-            ,TRUE LoadBasedActivity  -- even though I am joining it on Order audit log its still a load based activity because we are usig the TableColumnAudit table as a base. 
+            ,TRUE LoadBasedActivity                                                                      -- even though I am joining it on Order audit log its still a load based activity because we are usig the TableColumnAudit table as a base. 
             ,GREATEST
               (
                   IFNULL(LOAD_CTL.SourceAsOfTS, to_timestamp('1970-01-01 00:00:00')),
@@ -20,29 +41,22 @@ with New_Pronum_logic -- Temp for testing
                
         FROM
             OPERATIONS_DOMAIN.DBO.V_HVR_DBO_TABLECOLUMNAUDIT TCA
-            INNER JOIN OPERATIONS_DOMAIN.DBO.STG_SHIPMENT_ACTIVITY_LOADLIST LOAD_CTL 
+            INNER JOIN OPERATIONS_DOMAIN.DBO.STG_SHIPMENT_ACTIVITY_LOADLIST LOAD_CTL ON LOAD_CTL.LOADNUM = TCA.KeyColumnValue
           
-            LEFT  JOIN  ORION_RAP.OA.ORDERAUDIT OA on  OA.entityID = TCA.KeyColumnValue -- matching load num
-                                                   AND OA.NewValue = TCA.AuditColumnNewValue -- maching actual pronum as a new value
-                                                   AND Upper(Trim(OA.ENTEREDBYPARTYCODE))=Upper(Trim(TCA.AuditUserId)) --matching entred by
-                                                   AND OA.EntityTypeID = 2 -- 2 = loadnum 1 = ordernum
-                                                   AND OA.ACTIONITEMTYPEID  in (5,31) --- 5 = addrefnum -- 31 Update refnum
-                                                   AND OA.FIELDNAME = 'Value'
-                                                   AND OA.SOURCESYSTEMID in (479,3956) -- 479 =manual entry and 3956 = execution(done in load)
-                                                   AND OA.FIELDPARENT like ('T%')
-                                                   
-                        
+            LEFT  JOIN  ORDERAUDIT_PRONUM OA on  OA.LOADNUM = TCA.KeyColumnValue                                  -- matching load num
+                                                   AND OA.PRONUM = TCA.AuditColumnNewValue                             -- maching actual pronum as a new value
+                                                   AND Upper(Trim(OA.ENTEREDBY))=Upper(Trim(TCA.AuditUserId))   --matching entred by
+                
         WHERE 
                 TCA.TableName = 'LOADBOOKS'
             AND TCA.AuditColumnName = 'CARRIERPRONUMBER'
-            AND TCA.AuditDateTime >= '2021-05-01'--  for Testing 
-            AND TCA.AuditDateTime < '2021-05-07'--  For Testing 
-)
-
-
-
-,Shipment_Activity --- Creating a full set of shipment activity table for testing 
-(
+            AND TCA.AuditDateTime >= '2021-01-01'            --  for Testing 
+            AND TCA.AuditDateTime < '2021-04-01'             --  For Testing 
+  
+  )
+  
+  ,Shipment_Activity --- Creating a full set of shipment activity table for testing 
+  (
             Loadnum
             ,Activity
             ,ActivityDateTime
@@ -137,68 +151,43 @@ with New_Pronum_logic -- Temp for testing
 
 )
 
+    
+SELECT
+         a.employee
+         ,er.subrollupfamily
+         ,ba.branchname
+         ,a.mode
+        -- ,a.moderollup
+         ,a.activity 
+         ,tc.taskorder 
+         ,trim(upper(ba.branchsubregion1)) as BranchRegion
+         ,count(distinct a.loadnum) as loads
+         ,sum(a.activitycount) as ct
+       --  ,sum(a.activitycycletimeduration)/60 as hrs
+         
+         FROM Shipment_activity as a
 
-SELECT -- output
-a.Activity
-,a.Mode
-,a.activityDate
-,IsAutomated
-,Sum(a.activitycount) as Act_ct
+LEFT JOIN TRUCKLOAD_DOMAIN.DBO.V_DIM_BRANCH as ba on ba.branchID = a.employeebranchID 
+LEFT JOIN TRUCKLOAD_DOMAIN.DBO.V_DIM_CUSTOMER as c on c.customerid = a.customerid
+LEFT JOIN OPERATIONS_DOMAIN.DBO.DIM_TASK_CLASSIFICATION as tc on tc.task = a.activity
+LEFT JOIN  OPERATIONS_DOMAIN.DBO.DIM_EMPLOYEE_ROLE as er on trim(upper(a.employeeposition)) = trim(upper(er.psrole))
+WHERE 
 
- FROM Shipment_Activity a
-
-
-GROUP BY
-a.Activity
-,a.Mode
-,a.activityDate
-,IsAutomated
-
-
-
-
-
-
-
-//    
-//SELECT
-//         a.employee
-//         ,er.subrollupfamily
-//         ,ba.branchname
-//         ,a.moderollup
-//         ,a.activity 
-//         ,tc.taskorder 
-//         ,trim(upper(ba.branchsubregion1)) as BranchRegion
-//         ,count(distinct a.loadnum) as loads
-//         ,sum(a.activitycount) as ct
-//         ,sum(a.activitycycletimeduration)/60 as hrs
-//         ,Sum(case when NP.IsautomatedA =0 then a.activitycycletimeduration/60 else 0 end) as New_Logic_Hrs
-//         ,sum(NP.IsautomatedA) as New_Isautomated
-//FROM Shipment_activity as a
-//LEFT JOIN  New_Pronum_logic as NP  on a.loadnum = NP.Loadnum and a.employee = NP.employee and A.ActivityDatetime = NP.ActivityDatetime
-//
-//LEFT JOIN TRUCKLOAD_DOMAIN.DBO.V_DIM_BRANCH as ba on ba.branchID = a.employeebranchID 
-//LEFT JOIN TRUCKLOAD_DOMAIN.DBO.V_DIM_CUSTOMER as c on c.customerid = a.customerid
-//LEFT JOIN OPERATIONS_DOMAIN.DBO.DIM_TASK_CLASSIFICATION as tc on tc.task = a.activity
-//LEFT JOIN  OPERATIONS_DOMAIN.DBO.DIM_EMPLOYEE_ROLE as er on trim(upper(a.employeeposition)) = trim(upper(er.psrole))
-//WHERE 
-//
-// ba.primarybusinesslineid = 62
-// 
-//GROUP BY        
-//         a.employee
-//         ,er.subrollupfamily
-//         ,ba.branchname
-//         ,trim(upper(ba.branchsubregion1))
-//         ,a.moderollup
-//         ,a.activity 
-//         ,tc.taskorder 
-   
+ ba.primarybusinesslineid = 62
+ and a. IsAutomated = 'FALSE' 
  
+GROUP BY        
+         a.employee
+         ,er.subrollupfamily
+         ,ba.branchname
+         ,trim(upper(ba.branchsubregion1))
+       --  ,a.moderollup
+         ,a.mode
+         ,a.activity 
+         ,tc.taskorder 
 
-  
-  
-  
 
- 
+
+
+
   
